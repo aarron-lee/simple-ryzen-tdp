9b875993aa4013ee150f2c507976e7a5e45f64a3
Aarron Lee <aclee91@gmail.com>
2022-12-06 03:07:25 -0500

    adjust for 720p

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ let window;
 
 function createWindow() {
     window = new BrowserWindow({
-        width: 640,
-        height: 360,
+        width: 1280,
+        height: 720,
         show: false,
         webPreferences: {
             nodeIntegration: false,
