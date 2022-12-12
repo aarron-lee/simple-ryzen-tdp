1ab95db94dcedac338af1b68b553b11ed3a83984
Aarron Lee <aclee91@gmail.com>
2022-12-12 14:22:59 -0500

    remove preserved tdp reset in window closed callback

--- src/backend/main.js
+++ src/backend/main.js
@@ -206,8 +206,6 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
-  // reset preserved TDP
-  setItem(PRESERVED_TDP, undefined);
   if (process.platform !== "darwin") {
     app.quit();
   }
