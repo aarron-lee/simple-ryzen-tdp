0d719719a1168e956092f58107d6f6083382ce4a
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-09-04 21:42:23 -0400

    enable auto-detectiong of ryzenadj at /usr/bin/ryzenadj

--- configure.sh
+++ configure.sh
@@ -6,6 +6,18 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+if test -f /usr/bin/ryzenadj; then
+    echo "ryzenadj detected at /usr/bin/ryzenadj"
+    # Set up the sudo permissions
+    echo "Adding password free sudo access to ryzenadj for user ${SUDO_USER}"
+    cat <<-EOF > "/etc/sudoers.d/simple_ryzen_tdp_sudo"
+${SUDO_USER} ALL=(ALL) NOPASSWD: /usr/bin/ryzenadj*
+EOF
+    exit
+else
+    echo "ryzenadj not automatically detected"
+fi
+
 if [ "$1" == "" ]
   then echo "Please provide path to ryzenadj"
   exit
